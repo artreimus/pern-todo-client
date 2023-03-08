04a582221883e9d3bdec76b5f5dadb6fb61195d6
artreimus <63195930+artreimus@users.noreply.github.com>
2023-03-08 15:28:09 +0800

    add landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,35 +19,65 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <>
-        <div className="flex justify-center items-center flex-col h-screen">
-          <div className="mb-5">
-            <Logo />
+        <div className="h-screen bg-slate-100 ">
+          <div className="flex pt-4 items-center">
+            <div className="mb-3 ml-4  text-5xl ">
+              <Logo />
+            </div>
+            {auth.accessToken ? (
+              <div className="ml-auto text-md text-black font-medium">
+                <button
+                  className=" border border-black rounded-lg tracking-wider px-3 py-2 text-center mr-2 mb-2"
+                  onClick={() => router.push('/lists')}
+                >
+                  Go to App
+                </button>
+              </div>
+            ) : (
+              <div className="ml-auto text-md text-black font-medium">
+                <button
+                  className=" border border-black rounded-lg tracking-wider px-3 py-2 text-center mr-2 mb-2"
+                  onClick={() => router.push('/auth/login')}
+                >
+                  Login
+                </button>
+                <button
+                  className="ml-1 border border-black rounded-lg tracking-wider px-3 py-2 text-center mr-2 mb-2"
+                  onClick={() => router.push('/auth/register')}
+                >
+                  Register
+                </button>
+              </div>
+            )}
           </div>
-          {auth.accessToken ? (
+
+          <div className="flex justify-center items-center flex-col h-5/6">
+            <div className="text-5xl sm:text-6xl tracking-wider max-w-80 text-center font-bold mb-6">
+              <h2>
+                Effortlessly organize your tasks with
+                <span className="text-transparent bg-clip-text bg-gradient-to-r from-pink-400 to-purple-500">
+                  {' '}
+                  iDo
+                </span>
+              </h2>
+            </div>
+            <div className="text-md font-semibold max-w-80 text-center mb-4">
+              <p>
+                iDo is an elegant and intuitive todo application designed to
+                help users streamline their tasks, goals, and priorities. With
+                iDo, users can create custom lists, add tasks with due dates,
+                and manage their day or week with ease.
+              </p>
+            </div>
             <div>
               <button
-                className="border px-6 py-3"
+                className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-md px-5 py-2.5 tracking-wider text-center mr-2 mb-2"
                 onClick={() => router.push('/lists')}
               >
-                Go to App
+                Get Started
               </button>
             </div>
-          ) : (
-            <div>
-              <button
-                className="border mr-5 px-6 py-3"
-                onClick={() => router.push('/auth/login')}
-              >
-                Login
-              </button>
-              <button
-                className="border px-6 py-3"
-                onClick={() => router.push('/auth/register')}
-              >
-                Register
-              </button>
-            </div>
-          )}
+          </div>
         </div>
       </>
     </>
